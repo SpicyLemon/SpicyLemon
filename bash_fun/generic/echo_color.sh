ec069e449da7b57b63a933613d1d17f9f4c45934
Daniel Wedul <github@wedul.com>
2020-05-02 22:53:38 -0600

    Overhaul and enhance echo_color.

--- bash_fun/generic/echo_color.sh
+++ bash_fun/generic/echo_color.sh
@@ -5,17 +5,7 @@
 #
 # File contents:
 #   echo_color  ----------------------> Outputs a message using a specific color code.
-#   echo_white  ----------------------> Outputs a message in white.
-#   echo_red  ------------------------> Outputs a message in red.
-#   echo_green  ----------------------> Outputs a message in green.
-#   echo_yellow  ---------------------> Outputs a message in yellow.
-#   echo_blue  -----------------------> Outputs a message in blue.
-#   echo_pink  -----------------------> Outputs a message in pink.
-#   echo_cyan  -----------------------> Outputs a message in teal.
-#   echo_underline  ------------------> Outputs an underlined message.
-#   echo_strikethrough  --------------> Outputs a message with strikethrough.
-#   echo_bad  ------------------------> Outputs a message with bright red background and bright white text.
-#   show_colors  ---------------------> Outputs a chunk of color info.
+#   show_colors  ---------------------> Outputs color names and examples.
 #
 
 # Determine if this script was invoked by being executed or sourced.
@@ -24,140 +14,239 @@
   || [[ -n "$BASH_VERSION" ]] && (return 0 2>/dev/null) \
 ) && sourced='YES' || sourced='NO'
 
-# Usage: echo_color <color code> [-n] <message>
+# Usage: echo_color [<color name>|<color code>|-n|-N|--debug] -- <message>
 echo_color () {
-    local code_on debug newline_flag message code_off_parts code_on_part reset_to_default code_off
-    if [[ "$1" =~ ^[[:digit:]]+(\;[[:digit:]]+)*$ ]]; then
-        code_on="$1"
-        shift
-    else
-        echo -e "echo_color: Invalid color code: [$1]. Must have format <number>[;<number>[...]]." >&2
-        return 1
-    fi
-    if [[ "$1" == '--debug' ]]; then
-        debug='--debug'
-        shift
-    fi
-    if [[ "$1" == '-n' ]]; then
-        newline_flag='-n'
-        shift
-    fi
-    message="$@"
+    local usage
+    usage="$( cat << EOF
+echo_color - Makes it easier to output things in colors.
+
+Usage: echo_color [<color name>|<color code>|-n|-N|-d|--debug|--examples] -- <message>
+    <color name> can be one of:
+                    black  red  green  yellow  blue  magenta  cyan  light-gray
+                    dark-gray  light-red  light-green  light-yellow  light-blue  light-magenta  light-cyan  white
+                    bg-black  bg-red  bg-green  bg-yellow  bg-blue  bg-magenta  bg-cyan  bg-light-gray
+                    bg-dark-gray  bg-light-red  bg-light-green  bg-light-yellow  bg-light-blue  bg-light-magenta  bg-light-cyan  bg-white
+                    bold  dim  underline  strikethrough  reversed
+                    success  info  warn  error  good  bad
+    <color code> can be one or more numerical color code numbers separated by semicolons or spaces.
+                    These values will be formatted correctly and placed between \"<esc>[\" and \"m\" for output.
+                    I have found this page to be a good resource: https://misc.flogisoft.com/bash/tip_colors_and_formatting
+    -n signifies that you do not want a trailing newline added to the output.
+    -N signifies that you DO want a trailing newline added to the output. This is the default behavior.
+    --debug will cause extra messages to be sent to stderr that can help with debugging if needed.
+    --examples will cause the other parameters (except --debug) to be ignored, and instead output a set of examples.
+EOF
+)"
+    local code_on_parts code_off_parts without_newline debug show_examples code_on_part reset_to_default message
+    code_on_parts=()
     code_off_parts=()
-    for code_on_part in $( printf %s "$code_on" | tr ';' '\n' ); do
-        case "$code_on_part" in
-        1)              code_off_parts+=( 21 22 );;
-        2|4|7|9)        code_off_parts+=( $(( code_on_part + 20 )) );;
-        3[01234567])    code_off_parts+=( 39 );;
-        4[01234567])    code_off_parts+=( 49 );;
+    while [[ "$#" -gt '0' && "$1" != '--' ]]; do
+        case "$( printf %s "$1" | tr '[:upper:]' '[:lower:]' )" in
+        -h|--help|help)
+            echo "$usage"
+            return 0
+            ;;
+        black)                  code_on_parts+=( '30' ); code_off_parts+=( '39' ) ;;
+        red)                    code_on_parts+=( '31' ); code_off_parts+=( '39' ) ;;
+        green)                  code_on_parts+=( '32' ); code_off_parts+=( '39' ) ;;
+        yellow)                 code_on_parts+=( '33' ); code_off_parts+=( '39' ) ;;
+        blue)                   code_on_parts+=( '34' ); code_off_parts+=( '39' ) ;;
+        magenta|pink)           code_on_parts+=( '35' ); code_off_parts+=( '39' ) ;;
+        cyan|teal)              code_on_parts+=( '36' ); code_off_parts+=( '39' ) ;;
+        light-gray|light-grey)  code_on_parts+=( '37' ); code_off_parts+=( '39' ) ;;
+        dark-gray|dark-grey)                                    code_on_parts+=( '90' ); code_off_parts+=( '39' ) ;;
+        light-red|bright-red)                                   code_on_parts+=( '91' ); code_off_parts+=( '39' ) ;;
+        light-green|bright-green)                               code_on_parts+=( '92' ); code_off_parts+=( '39' ) ;;
+        light-yellow|bright-yellow)                             code_on_parts+=( '93' ); code_off_parts+=( '39' ) ;;
+        light-blue|bright-blue)                                 code_on_parts+=( '94' ); code_off_parts+=( '39' ) ;;
+        light-magenta|bright-magenta|light-pink|bright-pink)    code_on_parts+=( '95' ); code_off_parts+=( '39' ) ;;
+        light-cyan|bright-cyan|light-teal|bright-teal)          code_on_parts+=( '96' ); code_off_parts+=( '39' ) ;;
+        white)                                                  code_on_parts+=( '97' ); code_off_parts+=( '39' ) ;;
+        bg-black)                       code_on_parts+=( '40' ); code_off_parts+=( '49' ) ;;
+        bg-red)                         code_on_parts+=( '41' ); code_off_parts+=( '49' ) ;;
+        bg-green)                       code_on_parts+=( '42' ); code_off_parts+=( '49' ) ;;
+        bg-yellow)                      code_on_parts+=( '43' ); code_off_parts+=( '49' ) ;;
+        bg-blue)                        code_on_parts+=( '44' ); code_off_parts+=( '49' ) ;;
+        bg-magenta|bg-pink)             code_on_parts+=( '45' ); code_off_parts+=( '49' ) ;;
+        bg-cyan|bg-teal)                code_on_parts+=( '46' ); code_off_parts+=( '49' ) ;;
+        bg-light-gray|bg-light-grey)    code_on_parts+=( '47' ); code_off_parts+=( '49' ) ;;
+        bg-dark-gray|bg-dark-grey)                                          code_on_parts+=( '100' ); code_off_parts+=( '49' ) ;;
+        bg-light-red|bg-bright-red)                                         code_on_parts+=( '101' ); code_off_parts+=( '49' ) ;;
+        bg-light-green|bg-bright-green)                                     code_on_parts+=( '102' ); code_off_parts+=( '49' ) ;;
+        bg-light-yellow|bg-bright-yellow)                                   code_on_parts+=( '103' ); code_off_parts+=( '49' ) ;;
+        bg-light-blue|bg-bright-blue)                                       code_on_parts+=( '104' ); code_off_parts+=( '49' ) ;;
+        bg-light-magenta|bg-bright-magenta|bg-light-pink|bg-bright-pink)    code_on_parts+=( '105' ); code_off_parts+=( '49' ) ;;
+        bg-light-cyan|bg-bright-cyan|bg-light-teal|bg-bright-teal)          code_on_parts+=( '106' ); code_off_parts+=( '49' ) ;;
+        bg-white)                                                           code_on_parts+=( '107' ); code_off_parts+=( '49' ) ;;
+        bold)           code_on_parts+=( '1' ); code_off_parts+=( '21' '22' ) ;;
+        dim)            code_on_parts+=( '2' ); code_off_parts+=( '22' ) ;;
+        underline)      code_on_parts+=( '4' ); code_off_parts+=( '24' ) ;;
+        reversed)       code_on_parts+=( '7' ); code_off_parts+=( '27' ) ;;
+        strikethrough)  code_on_parts+=( '9' ); code_off_parts+=( '29' ) ;;
+        success)        code_on_parts+=( '97' '42' ); code_off_parts+=( '39' '49' ) ;;
+        info)           code_on_parts+=( '97' '100' ); code_off_parts+=( '39' '49' ) ;;
+        warn|warning)   code_on_parts+=( '93' '100' ); code_off_parts+=( '39' '49' ) ;;
+        error)          code_on_parts+=( '1' '91' '100' ); code_off_parts+=( '21' '22' '39' '49' ) ;;
+        good)           code_on_parts+=( '92' '100' ); code_off_parts+=( '39' '49' ) ;;
+        bad)            code_on_parts+=( '1' '97' '41' ); code_off_parts+=( '21' '22' '39' '49' ) ;;
+        -n) without_newline='YES' ;;
+        -N) without_newline= ;;
+        -d|--debug) debug='--debug' ;;
+        --examples) show_examples='--examples' ;;
         *)
-            reset_to_default='YES'
-            break
+            if [[ "$1" =~ ^[[:digit:]]+(([[:space:]]+|\;)[[:digit:]]+)*$ ]]; then
+                code_on_part="$( printf %s $1 | sed -E 's/[[:space:]]+/;/g' )"
+                code_on_parts+=( "$code_on_part" )
+                # TODO: Enhance this to handle multiple settings.
+                if [[ "$code_on_part" =~ ^38\;5\;[[:digit:]]+$ ]]; then
+                    code_off_parts+=( '39' )
+                elif [[ "$code_on_part" =~ ^48\;5\;[[:digit:]]+$ ]]; then
+                    code_off_parts+=( '49' )
+                else
+                    reset_to_default='YES'
+                fi
+            else
+                echo -e "echo_color: Invalid color name or code: [$1]." >&2
+                return 1
+            fi
             ;;
         esac
+        shift
     done
+    if [[ "$1" != '--' ]]; then
+        echo -E "No '--' separator found." >&2
+        return 1
+    fi
+    shift
+    message="$@"
+    # Allowing for an empty message because:
+    #   a) It won't really hurt anything.
+    #   b) It makes calling this function with user-defined input easier and nicer.
+    #   c) It'll make it easier to expand functionality to allow messages to be piped in.
+
+    if [[ -n "$show_examples" ]]; then
+        [[ "$debug" ]] && echo -E "Showing examples." >&2
+        show_colors "$debug"
+        return 0
+    fi
+
+    local code_on code_off newline_flag full_output
+    code_on="$( join_str ';' "${code_on_parts[@]}" )"
     if [[ -n "$reset_to_default" ]]; then
         code_off='0'
     else
-        code_off_parts=( $( echo "${code_off_parts[@]}" | tr ' ' '\n' | sort -n -u ) )
         code_off="$( join_str ';' "${code_off_parts[@]}" )"
     fi
-    [[ -n "$debug" ]] && { printf '%s' "\033[${code_on}m${message}\033[${code_off}m -> "; echo -e "[\033[${code_on}m${message}\033[${code_off}m]"; } >&2
-    echo -e $newline_flag "\033[${code_on}m${message}\033[${code_off}m"
-}
-
-# Usage: echo_white <string>
-echo_white () {
-    echo_color '1;37' "$@"
-}
-
-# Usage: echo_red <string>
-echo_red () {
-    echo_color '1;31' "$@"
-}
-
-# Usage: echo_green <string>
-echo_green () {
-    echo_color '1;32' "$@"
-}
-
-# Usage: echo_yellow <string>
-echo_yellow () {
-    echo_color '1;33' "$@"
-}
-
-# Usage: echo_blue <string>
-echo_blue () {
-    echo_color '1;34' "$@"
-}
-
-# Usage: echo_pink <string>
-echo_pink () {
-    echo_color '1;35' "$@"
-}
-
-# Usage: echo_cyan <string>
-echo_cyan () {
-    echo_color '1;36' "$@"
-}
-
-echo_underline () {
-    echo_color '4' "$@"
-}
-
-echo_strikethrough () {
-    echo_color '9' "$@"
-}
-
-echo_reversed () {
-    echo_color '7' "$@"
-}
-
-echo_bad () {
-    echo_color '1;38;5;231;48;5;196' "$@"
+    if [[ -n "$without_newline" ]]; then
+        newline_flag='-n'
+    fi
+    full_output="\033[${code_on}m${message}\033[${code_off}m"
+    if [[ -n "$debug" ]]; then
+        {
+            printf '  Opening code: [%s].\n' "$code_on"
+            printf '       Message: [%s].\n' "$message"
+            printf '  Closing code: [%s].\n' "$code_off"
+            printf 'Adding newline: [%s].\n' "$( [[ -n "$newline_flag" ]] && echo 'NO' || echo 'yes' )"
+            printf 'Without interpretation: [%s].\n' "$full_output"
+            printf '   With interpretation: [%b].\n' "$full_output"
+        } >&2
+    fi
+    echo -e $newline_flag "$full_output"
 }
 
 # Displays examples of some color codes
-# Usage: show_colors
+# Usage: show_colors [-v|--verbose] [-d|--debug] [-c|--combos]
 show_colors () {
-    local debug verbose codes output
-    if [[ "$1" == '--debug' ]]; then
-        debug='--debug'
+    local debug verbose show_combos text_colors background_colors effects special_formats fg_codes bg_codes effect_codes output
+    while [[ "$#" -gt '0' ]]; do
+        case "$( printf '%s' "$1" | tr '[:upper:]' '[:lower:]' )" in
+        -v|--verbose)   verbose='--verbose' ;;
+        -d|--debug)     debug='--debug' ;;
+        -c|--combos)    show_combos='--combos' ;;
+        *)
+            echo "Unknown parameter: [$1]." >&2
+            return 1
+            ;;
+        esac
         shift
-    elif [[ "$1" == '-v' || "$1" == '--verbose' ]]; then
-        verbose='-v'
-        shift
-    fi
-    codes=( '1' '2' '4' '7' '9' $( seq 30 37 ) $( seq 40 47 ) )
+    done
+    text_colors=(
+        black      red            blue        green
+        dark-gray  light-red      light-blue  light-green
+        light-gray magenta        cyan        yellow
+        white      light-magenta  light-cyan  light-yellow
+    )
+    background_colors=(
+        bg-black      bg-red            bg-blue        bg-green
+        bg-dark-gray  bg-light-red      bg-light-blue  bg-light-green
+        bg-light-gray bg-magenta        bg-cyan        bg-yellow
+        bg-white      bg-light-magenta  bg-light-cyan  bg-light-yellow
+    )
+    effects=(
+        bold  dim  underline  strikethrough  reversed
+    )
+    special_formats=(
+        success  info  warn  error  good  bad
+    )
+    fg_codes=( 30 90 37 97 31 91 35 95 34 94 36 96 32 92 33 93 )
+    bg_codes=( 40 100 47 107 41 101 45 105 44 104 46 106 42 102 43 103 )
     output="$(
-        # Sneaky private function with access to variables from parent function. Teehee.
-        output_section () {
-            local title base_code width padl padr format sub_code code
+        # Sneaky private functions with access to variables from parent function. Teehee.
+        output_simple_section () {
+            local title name_width per_line i text_color
             title="$1"
-            base_code="$2"
-            width="$(( ${#base_code} + 2 ))"
-            padl="$( printf "% $(( ( 6 - width ) / 2 ))s" '' )"
-            padr="$( printf "% $(( ( 6 - width + 1 ) / 2 ))s" '' )"
-            format="%-${width}s"
-            printf '%18s' "$title: "
-            for sub_code in "${codes[@]}"; do
-                code="${base_code}${sub_code}"
-                echo -n '['
-                echo_color "$code" $debug -n "###${padl} $( printf $format $code ) ${padr}###"
-                echo -n ']'
-                if [[ "$sub_code" -eq '9' || "$sub_code" -eq '37' || "$sub_code" -eq '47' ]]; then
-                    printf '\n'
-                else
-                    printf %s '  '
+            shift
+            name_width="$1"
+            shift
+            per_line="$1"
+            shift
+            echo -E "$title:"
+            i=0
+            for color in "$@"; do
+                i=$(( i + 1 ))
+                echo -n ' '
+                if [[ "$i" -eq '1' ]]; then
+                    echo -n ' '
+                fi
+                printf "%${name_width}s:[" "$color"
+                echo_color -n "$color" $debug -- " Example "
+                printf ']'
+                if [[ "$i" -eq "$per_line" ]]; then
+                    i=0
+                    echo ''
                 fi
             done
+            [[ "$i" -ne '0' ]] && echo ''
         }
-        output_section "Normal"        ''
-        output_section "Reversed"      '7;'
-        output_section "Bold"          '1;'
-        output_section "Bold Reversed" '1;7;'
-        output_section "Dim"           '2;'
-        output_section "Dim Reversed"  '2;7;'
+        output_combo_section () {
+            local title added_code pad fg_code bg_code code
+            title="$1"
+            added_code="$2"
+            if [[ -n "$added_code" ]]; then
+                added_code="${added_code};"
+            else
+                pad=' '
+            fi
+            echo -E "$title:"
+            for fg_code in "${fg_codes[@]}"; do
+                printf ' '
+                for bg_code in "${bg_codes[@]}"; do
+                    code="${added_code}${fg_code};${bg_code}"
+                    printf ' %-9b' "\033[${code}m${pad}${code}${pad}\033[0m"
+                done
+                echo ''
+            done
+        }
+        output_simple_section 'Text Colors'       '16' '4' "${text_colors[@]}"
+        output_simple_section 'Background Colors' '16' '4' "${background_colors[@]}"
+        output_simple_section 'Text Effects'      '1'  '9' "${effects[@]}"
+        output_simple_section 'Special Formats'   '1'  '9' "${special_formats[@]}"
+        if [[ -n "$show_combos" ]]; then
+            output_combo_section 'Color Combos'            ''
+            output_combo_section 'Color Combos - Bold'     '1'
+            output_combo_section 'Color Combos - Dim'      '2'
+            output_combo_section 'Color Combos - Reversed' '7'
+        fi
     )"
     if [[ -n "$debug" || -n "$verbose" ]]; then
         {
