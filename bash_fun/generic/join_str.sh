c577afa65539629711225ec57b6db90776155f10
Daniel Wedul <github@wedul.com>
2020-05-03 03:24:38 -0600

    Create join_str.sh.

---
+++ bash_fun/generic/join_str.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# This file contains the join_str function that joins multiple values using a delimiter.
+# This file can be sourced to add the join_str function to your environment.
+# This file can also be executed to run the join_str function without adding it to your environment.
+#
+# File contents:
+#   join_str  --> Joins a list of parameters using a delimiter.
+#
+
+# Determine if this script was invoked by being executed or sourced.
+( [[ -n "$ZSH_EVAL_CONTEXT" && "$ZSH_EVAL_CONTEXT" =~ :file$ ]] \
+  || [[ -n "$KSH_VERSION" && $(cd "$(dirname -- "$0")" && printf '%s' "${PWD%/}/")$(basename -- "$0") != "${.sh.file}" ]] \
+  || [[ -n "$BASH_VERSION" ]] && (return 0 2>/dev/null) \
+) && sourced='YES' || sourced='NO'
+
+# Uses the provided delimiter to join all the provided arguments.
+# Usage: join_str <delimiter> [<arg1> [<arg2>... ]]
+join_str () {
+    local d retval
+    d="$1"
+    shift
+    retval="$1"
+    shift
+    while [[ "$#" -gt '0' ]]; do
+        retval="${retval}${d}${1}"
+        shift
+    done
+    printf %s "$retval"
+}
+
+if [[ "$sourced" != 'YES' ]]; then
+    join_str "$@"
+    exit $?
+fi
+unset sourced
+
+return 0
