490d0441ff63558af7fa6cd4b3c7ad9dd82b1f8c
Daniel Wedul <github@wedul.com>
2020-05-04 15:53:27 -0600

    Tweak the error message for files that were executed instead of being sourced.

--- bash_fun/generic-setup.sh
+++ bash_fun/generic-setup.sh
@@ -17,8 +17,7 @@
 if [[ "$sourced" != 'YES' ]]; then
     >&2 cat << EOF
 This script is meant to be sourced instead of executed.
-Please run this command to enable the functionality contained within.
-$( echo -e "\033[1;37msource $( basename "$0" 2> /dev/null || basename "$BASH_SOURCE" )\033[0m" )
+Please run this command to enable the functionality contained in within: $( printf '\033[1;37msource %s\033[0m' "$( basename "$0" 2> /dev/null || basename "$BASH_SOURCE" )" )
 EOF
     exit 1
 fi
