150e7294c97a1a5824d24d060e5c95938e06a092
Daniel Wedul <github@wedul.com>
2021-03-11 09:52:21 -0700

    Update the git_diff_analysis to be off the 'main' branch by default and be tailored for go code instead of java.

--- bash_fun/git/git_diff_analysis.sh
+++ bash_fun/git/git_diff_analysis.sh
@@ -20,7 +20,7 @@ git_diff_analysis - Gets some stats on branch differences.
 
 Usage: git_diff_analysis [<main branch> [<branch with changes>]]
 
-    If no branches are supplied, the diff will be from master to your current branch.
+    If no branches are supplied, the diff will be from main to your current branch.
     If only one branch is supplied, the diff will be from that branch to your current branch.
     If two brances are supplied, the diff will be from the first branch to the second.
 EOF
@@ -48,7 +48,7 @@ EOF
     elif [[ "${#branches[@]}" -eq '1' ]]; then
         branches+=( "$( git_branch_name )" )
     elif [[ "${#branches[@]}" -eq '0' ]]; then
-        branches=( 'master' "$( git_branch_name )" )
+        branches=( 'main' "$( git_branch_name )" )
     fi
     if ! git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
         printf 'This command must be run from a git folder.\n' >&2
@@ -65,8 +65,8 @@ EOF
     diff_numstats_cmd=( git diff ${branches[@]} --numstat )
     [[ -n "$verbose" ]] && printf '  \033[97m%s\033[0m\n' "${diff_numstats_cmd[*]}" >&2
     diff_numstats="$( "${diff_numstats_cmd[@]}" )"
-    test_entries="$( grep 'src/test' <<< "$diff_numstats" )"
-    code_entries="$( grep -v 'src/test' <<< "$diff_numstats" )"
+    test_entries="$( grep "_test\.go$" <<< "$diff_numstats" )"
+    code_entries="$( grep -v "_test\.go$" <<< "$diff_numstats" )"
 
     total_lines_added="$( awk '{sum+=$1} END { print sum }' <<< "$diff_numstats" )"
     total_lines_removed="$( awk '{sum-=$2} END { print sum }' <<< "$diff_numstats" )"
@@ -74,17 +74,6 @@ EOF
     test_lines_removed="$( awk '{sum-=$2} END { print sum }' <<< "$test_entries" )"
     code_lines_added="$( awk '{sum+=$1} END { print sum }' <<< "$code_entries" )"
     code_lines_removed="$( awk '{sum-=$2} END { print sum }' <<< "$code_entries" )"
-
-    [[ -n "$verbose" ]] && printf '\033[96mCounting unit test changes.\033[0m\n' >&2
-
-    diff_no_context_cmd=( git diff ${branches[@]} -U0 )
-    diff_tests_filter=( grep '@Test' )
-    [[ -n "$verbose" ]] && printf '  \033[97m%s | %s\033[0m\n' "${diff_no_context_cmd[*]}" "${diff_tests_filter[*]}" >&2
-    diff_tests="$( "${diff_no_context_cmd[@]}" | "${diff_tests_filter[@]}" )"
-
-    tests_added="$( awk '{ if ($0 ~ /^\+/) sum+=1; } END { print sum; }' <<< "$diff_tests" )"
-    tests_removed="$( awk '{ if ($0 ~ /^\-/) sum-=1; } END { print sum; }' <<< "$diff_tests" )"
-    tests_delta=$(( tests_added + tests_removed ))
 
     [[ -n "$verbose" ]] && printf '\033[96mDone.\033[0m\n' >&2
     (
@@ -97,9 +86,6 @@ EOF
         printf 'Line Changes -  Code: %+6d  %+6d\n' "$code_lines_added" "$code_lines_removed"
         printf 'Line Changes - Tests: %+6d  %+6d\n' "$test_lines_added" "$test_lines_removed"
         printf 'Line Changes - Total: %+6d  %+6d\n' "$total_lines_added" "$total_lines_removed"
-        printf 'Unit Tests -   Added: %+6d\n' "$tests_added"
-        printf 'Unit Tests - Removed: %+6d\n' "$tests_removed"
-        printf 'Unit Tests -   Delta: %+6d\n' "$tests_delta"
     )
 }
 
