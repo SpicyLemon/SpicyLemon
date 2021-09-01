eb2cb3188630332f3efa6446019b2f3217034654
Daniel Wedul <github@wedul.com>
2021-09-01 14:44:16 -0600

    Swap out usage of echo for print in change_word.sh. Also allow for defining the colors to use with env vars. And lastly, fix the multi-file functionality.

--- bash_fun/generic/change_word.sh
+++ bash_fun/generic/change_word.sh
@@ -15,40 +15,56 @@
 
 # Usage: change_word <old word> <new word> <files>
 change_word () {
-    local old_word new_word files file
+    local old_word new_word files old_color new_color before_color after_color file
     old_word="$1"
     new_word="$2"
     shift
     shift
-    files="$@"
+    files=( "$@" )
+    old_color="${OLD_COLOR:-31}"    # Default to red text
+    new_color="${NEW_COLOR:-32}"    # Default to green text
+    if [[ -z "$old_color" || "$old_color" =~ [^[:digit:]] || "$old_color" -lt '30' || "$old_color" -gt '37' ]]; then
+        printf 'Illegal OLD_COLOR: [%s]. It must be a number between 30 and 37 (inclusive).\n' >&2
+    fi
+    if [[ -z "$new_color" || "$new_color" =~ [^[:digit:]] || "$new_color" -lt '30' || "$new_color" -gt '37' ]]; then
+        printf 'Illegal NEW_COLOR: [%s]. It must be a number between 30 and 37 (inclusive).\n' >&2
+    fi
+    # The headings will be bold with grey text, and a backround matching the provided colors.
+    # The background color escape codes for each color are 10 more than their text color escape codes.
+    # E.g. 41 for a red background and 42 for a green background.
+    printf -v before_color '1;37;%d' "$(( old_color + 10 ))"
+    printf -v after_color '1;37;%d' "$(( new_color + 10 ))"
+    # Now, make the text colors bold
+    old_color="1;$old_color"
+    new_color="1;$new_color"
     if [[ -z "$old_word" || -z "$new_word" || "${#files[@]}" -eq '0' ]]; then
-        echo "Usage: change_word <old word> <new word> <files>"
+        printf 'Usage: change_word <old word> <new word> <files>\n'
         return 0
     fi
     if [[ "$old_word" =~ [^[:alnum:][:space:]\-_] ]]; then
-        echo "Only letters, numbers, dashes, underscores and spaces are allowed in the provided words." >&2
+        printf 'Only letters, numbers, dashes, underscores and spaces are allowed in the provided words.\n' >&2
         return 1
     fi
     if [[ "$new_word" =~ [^[:alnum:][:space:]\-_] ]]; then
-        echo "Only letters, numbers, dashes, underscores and spaces are allowed in the provided words." >&2
+        printf 'Only letters, numbers, dashes, underscores and spaces are allowed in the provided words.\n' >&2
         return 1
     fi
-    echo -e "Changing \033[0m \033[1;31m$old_word\033[0m to \033[1;32m$new_word\033[0m"
-    echo -e "In: ${files[@]}"
-    echo ''
-    echo -e "\033[1;37;41m Before: \033[0m"
+    printf 'Changing \033[%sm%s\033[0m to \033[%sm%s\033[0m\n' "$old_color" "$old_word" "$new_color" "$new_word"
+    printf 'In: %s\n' "${files[*]}"
+    printf '\n'
+    printf '\033[%sm Before: \033[0m\n' "$before_color"
     grep -E "\<($old_word|$new_word)\>" "${files[@]}" \
-        | GREP_COLOR='1;31' grep --color=always "\<$old_word\>\|$" \
-        | GREP_COLOR='1;32' grep --color=always "\<$new_word\>\|$"
-    echo ''
-    for file in ${files[@]}; do
+        | GREP_COLOR="$old_color" grep --color=always "\<$old_word\>\|$" \
+        | GREP_COLOR="$new_color" grep --color=always "\<$new_word\>\|$"
+    printf '\n'
+    for file in "${files[@]}"; do
         sed -i '' "s/[[:<:]]$old_word[[:>:]]/$new_word/g;" "$file"
     done
-    echo -e "\033[1;37;42m After: \033[0m"
+    printf '\033[%sm After: \033[0m\n' "$after_color"
     grep -E "\<($old_word|$new_word)\>" "${files[@]}" \
-        | GREP_COLOR='1;31' grep --color=always "\<$old_word\>\|$" \
-        | GREP_COLOR='1;32' grep --color=always "\<$new_word\>\|$"
-    echo ''
+        | GREP_COLOR="$old_color" grep --color=always "\<$old_word\>\|$" \
+        | GREP_COLOR="$new_color" grep --color=always "\<$new_word\>\|$"
+    printf '\n'
 }
 
 if [[ "$sourced" != 'YES' ]]; then
