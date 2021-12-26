e7a93ffe3fb4d7f1bb31257445c49a2825207a0d
Daniel Wedul <github@wedul.com>
2021-12-25 22:11:30 -0700

    Add note files for days 11, 12, 13, and 14.

---
+++ advent_of_code/2015/adventofcode11.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+mainProgram();
+print "Done\n";
+exit(0);
+
+sub mainProgram {
+   print "I have credit for this, but the program has been lost to time.\n";
+   print "part 1: vzbxxyzz\n";
+   print "part 2: vzcaabcc\n";
+}
---
+++ advent_of_code/2015/adventofcode12.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+mainProgram();
+print "Done\n";
+exit(0);
+
+sub mainProgram {
+   print "I have credit for this, but the program has been lost to time.\n";
+   print "part 1: 156366\n";
+   print "part 2: 96852\n";
+}
