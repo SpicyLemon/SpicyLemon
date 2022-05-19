6faf02a6089a5847df4a34d50d6c0d6afe35f9ed
Daniel Wedul <github@wedul.com>
2022-05-19 17:52:37 -0600

    Updagte git_commit_diff to allow providing arguments to the git diff command.

--- bash_fun/git/git_commit_diff.sh
+++ bash_fun/git/git_commit_diff.sh
@@ -16,36 +16,66 @@
 git_commit_diff () {
     local usage
     usage="$( cat << EOF
-git_commit_diff - Shows the diff for a commit.
+git_commit_diff - Shows the diff for one or more commits.
 
-Usage: git_commit_diff (-s|--select|<commit hash> [<commit hash 2> ...])
+Usage: git_commit_diff [git diff args] [--select] [--commit <commit hash>] [--hash <commit hash>] [-- <commit hash> ...]
 
-    If -s or --select is supplied, you will be prompted to select the commit(s) to show.
-    Otherwise, the provided arguments will be treated as the commit hashes to use.
+    If --select is supplied, you will be prompted to select the commit(s) to show.
+    Multiple commit hashes can be provided using multiple --commit or --hash options.
+    Anything provided after -- will also be used as a commit hash.
+    All other arguments are provided to the git diff command.
 
 EOF
 )"
-    if [[ "$#" == '0' || "$1" == '-h' || "$1" == '--help' ]]; then
-        printf '%s\n' "$usage"
-        return 0
-    fi
-    local commit_hashes zwnj commit_hash return_code exit_code
-    if [[ "$1" == '-s' || "$1" == '--select' ]]; then
+    local do_select diff_args commit_hashes zwnj commit_hash return_code exit_code
+    diff_args=()
+    commit_hashes=()
+    while [[ "$#" -gt '0' ]]; do
+        case "$1" in
+            -h|--help)
+                printf '%s\n' "$usage"
+                return 0
+                ;;
+            --select)
+                do_select=YES
+                ;;
+            --commit|--hash)
+                if [[ -z "$2" ]]; then
+                    printf 'No hash provided after %s\n' "$1" >&2
+                fi
+                commit_hashes+=( "$2" )
+                shift
+                ;;
+            --)
+                shift
+                commit_hashes+=( "$@" )
+                set --
+                break
+                ;;
+            *)
+                diff_args+=( "$1" )
+                ;;
+        esac
+        shift
+    done
+    if [[ -n "$do_select" ]]; then
         zwnj="$( printf '\xe2\x80\x8b' )"
-        commit_hashes=(
+        commit_hashes+=(
             $( git log --date=format:'%F %T %A' --format=format:"%H${zwnj}%h  %<(30)%ad %an${zwnj}%<(60,trunc)%s" \
                 | fzf -m --cycle --tac --with-nth=2,3 --delimiter="$zwnj" --header=' hash     commit date                    author          subject' \
                 | sed -E 's/([[:xdigit:]]+).*$/\1/;' )
         )
-    else
-        commit_hashes=( "$@" )
+    fi
+    if [[ "${#commit_hashes[@]}" -eq '0' ]]; then
+        printf '%s\n' "$usage"
+        return 0
     fi
     return_code=0
     for commit_hash in "${commit_hashes[@]}"; do
         printf '> git log "%s" -n 1\n' "$commit_hash" \
             && git log "$commit_hash" -n 1 \
-            && printf '\n> git --no-pager diff "%s~" "%s"\n' "$commit_hash" "$commit_hash" \
-            && git --no-pager diff "$commit_hash~" "$commit_hash"
+            && printf '\n> git --no-pager diff %s "%s~" "%s"\n' "${diff_args[*]}" "$commit_hash" "$commit_hash" \
+            && git --no-pager diff "${diff_args[@]}" "$commit_hash~" "$commit_hash"
         exit_code=$?
         if [[ $exit_code -ne 0 ]]; then
             return_code=$exit_code
