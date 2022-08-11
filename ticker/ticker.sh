edc580f535506388b84df0d9b6e7d2b7ea2e63ba
Daniel Wedul <github@wedul.com>
2022-08-11 14:17:51 -0600

    Update the ticker.sh script.
    - Enhance the help/usage.
    - Lowercase all local variables.
    - Allow color customization of all the columns.
    - Look for a --help or -h flag.
    - Check for jq before doing anything at all.
    - Put a comment blurb at the top about the script.
    - Use an exported DEBUG env var instead of setting it in the script.

--- ticker/ticker.sh
+++ ticker/ticker.sh
@@ -1,12 +1,48 @@
 #!/bin/bash
+# This script uses the Yahoo Finance API to get ticker quotes, then formats and outputs the results.
+# This script is designed to be executed (as opposed to being sourced).
+# To install it, place it in one of your PATH directories.
+#
+# To get debug information, set the DEBUG environment variable to 1 (or anything other than an empty string, really).
 
-if [[ "$#" -eq '0' ]]; then
-    echo "Usage: ./ticker.sh AAPL MSFT GOOG DOGE-USD"
-    exit
+if ! command -v 'jq' > /dev/null 2>&1; then
+    printf 'Missing required command: jq\n' >&2
+    jq >&2  # Possibly provides shell/system specific information about the missing command.
+    exit $?
 fi
 
-SYMBOLS=()
-SYMBOLS_TO_GET=()
+print_usage () {
+    cat << EOF
+ticker.sh - Get ticker info from the Yahoo Finance API and format it nicely.
+
+Usage: ./ticker.sh <symbol> [<symbol 2> ...]
+
+Special "symbols":
+    These special symbols should be wrapped in [], (), {} or <> (whichever is easiest in your environment).
+    Depending on your environment, you may also need to quote them as arguments.
+        br - Adds an empty line to the output. E.g. '[br]'
+        hr - Adds a line of dashes to the output. E.g. '(hr)'
+
+Example:
+> ./ticker AAPL MSFT GOOG '[hr]' DOGE-USD
+
+The following environment variables can control things:
+    DEBUG    - Set to a non-empty string to output debug information.
+    NO_COLOR - Set to a non-empty string to not use coloring.
+    COLOR_SYMBOL    - Color code for the symbol.                Default: ''
+    COLOR_AMOUNT    - Color code for the amount.                Default: '1;37' (bold white)
+    COLOR_UP        - Color code for a positive diff.           Default: '32' (green)
+    COLOR_EVEN      - Color code for a zero diff.               Default: ''
+    COLOR_DOWN      - Color code for a negative diff.           Default: '31' (red)
+    COLOR_INDICATOR - Color code for the market indicator.      Default: ''
+    COLOR_NAME      - Color code for the name.                  Default: ''
+    COLOR_RESET     - Color code for ending a color section.    Default: '0' (reset all color aspects)
+EOF
+
+}
+
+symbols=()
+symbols_to_get=()
 
 hr='<hr>'
 br='<br>'
@@ -15,42 +51,55 @@ while [[ "$#" -gt '0' ]]; do
     # Check for special cases <br> and <hr> allowing for multiple bracket styles and casing.
     # Techinically this will catch stuff like <bR], but this reads better than trying to match stuff,
     # and catching extra stuff like that isn't going to hurt anything.
-    if [[ "$1" =~ ^[\[\(\{\<][bB][rR][\]\)\}\>]$ ]]; then
-        SYMBOLS+=( "$br" )
+    if [[ "$1" == '--help' || "$1" == '-h' ]]; then
+        help_requested=1
+    elif [[ "$1" =~ ^[\[\(\{\<][bB][rR][\]\)\}\>]$ ]]; then
+        symbols+=( "$br" )
     elif [[ "$1" =~ ^[\[\(\{\<][hH][rR][\]\)\}\>]$ ]]; then
-        SYMBOLS+=( "$hr" )
+        symbols+=( "$hr" )
     else
-        SYMBOLS+=( $1 )
-        SYMBOLS_TO_GET+=( $1 )
+        symbols+=( $1 )
+        symbols_to_get+=( $1 )
     fi
     shift
 done
 
-
-# Set this to anything non-empty to cause some debug statements to be printed along the way.
-DEBUG=
-
-if ! command -v 'jq' > /dev/null 2>&1; then
-    printf 'Missing required command: jq\n' >&2
-    jq >&2  # Possibly provides shell/system specific information about the missing command.
-    exit $?
+if [[ "${#symbols_to_get[@]}" -eq '0' ]]; then
+    print_usage
+    exit 0
 fi
+if [[ -n "$help_requested" ]]; then
+    print_usage
+fi
+
+to_escape_code () {
+    local val
+    val="$( sed -E 's/^[[:space:]]+//; s/[[:space:]]$//;' <<< "$1" )"
+    if [[ "${val:=$2}" =~ ^[[:digit:]\;]+$ ]]; then
+        val="\e[${val}m"
+    fi
+    printf '%b' "$val"
+}
 
 if [[ -z "$NO_COLOR" ]]; then
-    : "${COLOR_BOLD:=\e[1;37m}"
-    : "${COLOR_GREEN:=\e[32m}"
-    : "${COLOR_RED:=\e[31m}"
-    : "${COLOR_RESET:=\e[0m}"
+    c_sym="$( to_escape_code "$COLOR_SYMBOL" '' )"
+    c_amt="$( to_escape_code "$COLOR_AMOUNT" '1;37' )"
+    c_up="$( to_escape_code "$COLOR_UP" '32' )"
+    c_even="$( to_escape_code "$COLOR_EVEN" '' )"
+    c_down="$( to_escape_code "$COLOR_DOWN" '31' )"
+    c_ind="$( to_escape_code "$COLOR_INDICATOR" '' )"
+    c_name="$( to_escape_code "$COLOR_NAME" '' )"
+    c_rst="$( to_escape_code "$COLOR_RESET" '0' )"
 fi
 
-API_ENDPOINT="https://query1.finance.yahoo.com/v7/finance/quote?lang=en-US&region=US&corsDomain=finance.yahoo.com"
-FIELDS=(symbol shortName marketState regularMarketPrice regularMarketChange regularMarketChangePercent \
+api_endpoint="https://query1.finance.yahoo.com/v7/finance/quote?lang=en-US&region=US&corsDomain=finance.yahoo.com"
+fields=(symbol shortName marketState regularMarketPrice regularMarketChange regularMarketChangePercent \
   preMarketPrice preMarketChange preMarketChangePercent postMarketPrice postMarketChange postMarketChangePercent)
 
-url_symbols=$( IFS=,; echo "${SYMBOLS_TO_GET[*]}" )
-url_fields=$( IFS=,; echo "${FIELDS[*]}" )
+url_symbols=$( IFS=,; echo "${symbols_to_get[*]}" )
+url_fields=$( IFS=,; echo "${fields[*]}" )
 
-api_url="$API_ENDPOINT&fields=$url_fields&symbols=$url_symbols"
+api_url="$api_endpoint&fields=$url_fields&symbols=$url_symbols"
 
 [[ -n "$DEBUG" ]] && printf 'api_url: [%s]\n' "$api_url"
 
@@ -79,7 +128,7 @@ if command -v 'tput' > /dev/null 2>&1; then
     width="$( tput cols )"
 fi
 
-for symbol in ${SYMBOLS[*]}; do
+for symbol in ${symbols[*]}; do
     if [[ "$symbol" == "$br" ]]; then
         printf '\n'
         continue
@@ -100,11 +149,11 @@ for symbol in ${SYMBOLS[*]}; do
     read symbol price diff percent marketStateIndicator shortName <<< "$symbolLine"
 
     if [[ "$diff" =~ ^0(\.0*)?$ ]]; then
-        color=
+        c_diff="$c_even"
     elif [[ "$diff" =~ ^- ]]; then
-        color=$COLOR_RED
+        c_diff="$c_down"
     else
-        color=$COLOR_GREEN
+        c_diff="$c_up"
     fi
 
     if [[ "$marketStateIndicator" == '=' ]]; then
@@ -120,14 +169,16 @@ for symbol in ${SYMBOLS[*]}; do
 
     # Line Breakdown:
     #   index  format    len  description
+    # symbol coloring on
     #   1-10   '%-10s'   10   Ticker symbol, left justified.
+    # color reset
     #   11     ' '       1    Space.
-    # bold coloring on
+    # amount coloring on
     #   12-24  '%13.6f'  13   Value: 6 whole digits, 1 decimal, 6 fractional digits.
     #                         This will be more than 13 characters for values over 999,999.99999.
     # color reset
     #   25     ' '       1    Space.
-    # up(green)/down(red) coloring on
+    # up/even/down coloring on
     #   26-38  '%+13.6f' 13   value change: 1 +/- sign, 5 whole digits, 1 decimal, 6 fractional digits.
     #                         This will be more than 13 characters for values over 99,999.999999.
     #   39     ' '       1    Space.
@@ -138,8 +189,10 @@ for symbol in ${SYMBOLS[*]}; do
     #   48     ' '       1    Space.
     #   49     '%1s'     1    Market state indicator character: ' ', '<', or '>'.
     #   50     ' '       1    Space.
+    # name coloring on
     #   51-80  '[%s]'    30   Short name: 1 open bracket, up to 28 for shortname, 1 close bracket.
     #                         This might not take up the whole 30 characters allotted for it.
-    printf "%-10s $COLOR_BOLD%13.6f$COLOR_RESET $color%+13.6f %+7.2f%%$COLOR_RESET %1s [%s]\n" \
+    # color reset
+    printf "$c_sym%-10s$c_rst $c_amt%13.6f$c_rst $c_diff%+13.6f %+7.2f%%$c_rst $c_ind%1s$c_rst $c_name[%s]$c_rst\n" \
         "$symbol" "$price" "$diff" "$percent" "$marketStateIndicator" "$shortNameT"
 done
