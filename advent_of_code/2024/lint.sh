a0c3c6dc93e1e75c6df9ec7992d59f1d7e3f8667
Daniel Wedul <github@wedul.com>
2024-12-01 01:25:48 -0700

    Create the 2024 AoC dir.

---
+++ advent_of_code/2024/lint.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# This script will run the linter on the files in this dir.
+
+while [[ "$#" -gt '0' ]]; do
+    case "$1" in
+    --help|-h)
+        printf 'Usage: %s [--fix]\n' "$0"
+        exit 0
+        ;;
+    --fix)
+        fix='YES'
+        ;;
+    default)
+        printf 'Usage: %s [--fix]\n' "$0"
+        printf 'Unknown arg: %q\n' "$1"
+        exit 1
+        ;;
+    esac
+    shift
+done
+
+scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+gofmtcmd=( gofmt -s )
+lintcicmd=( golangci-lint run --config "$scriptDir/.golangci.yml" )
+if [[ -n "$fix" ]]; then
+    gofmtcmd+=( -w )
+    lintcicmd+=( --fix )
+else
+    gofmtcmd+=( -d )
+fi
+
+for f in $( find . -type f -name '*.go' | sort ); do
+    printf '%s gofmt ... ' "$f"
+    "${gofmtcmd[@]}" "$f"
+    printf 'golangci-lint ... '
+    "${lintcicmd[@]}" "$f"
+    printf 'Done\n'
+done
+
